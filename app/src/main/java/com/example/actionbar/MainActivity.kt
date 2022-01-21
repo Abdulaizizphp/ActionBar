0c8dd97c87f92f770af1241a25a3e198c3270ca7
Abdulaizizphp <95176633+Abdulaizizphp@users.noreply.github.com>
2022-01-21 17:58:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/actionbar/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.actionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.share_button){
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
